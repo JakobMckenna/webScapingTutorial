e0b7ab1b0bd347752d4c8935cac21fc205216204
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-07 15:23:18 -0500

    Added python code

---
+++ webScraper.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+result1 = soup.find(id="ResultsContainer")
+job_elements = result1.find_all("div", class_="card-content")
+
+print("Printing every single job listing: \n")
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    print()
+
+print("Filtering for specific python jobs: \n")
+python_jobs = result1.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
+
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+for job_element in python_job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    print()
+
+print("End of program")
